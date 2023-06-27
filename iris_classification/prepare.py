fec105ea0ae3d7a99da04b1ba8549f030f07007b
Eric Dasse <eric.dasse@stonal.com>
2023-06-27 14:04:48 +0200

    Update preparation script

--- iris_classification/prepare.py
+++ iris_classification/prepare.py
@@ -1,9 +1,19 @@
+from pathlib import Path
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 from iris_classification.settings import PREPARED_DATA_PATH, RAW_DATA_PATH
 
 
-def prepare(raw_data_path, prepared_data_path):
+def save_as_csv(X, y, destination):
+    iris_features = pd.Series(X)
+    labels = pd.Series(y)
+
+    iris_data = pd.concat([iris_features, labels])
+    iris_data.to_csv(destination)
+
+
+def prepare(raw_data_path: Path, prepared_data_dir: Path):
     # Load raw dataset
     iris = pd.read_csv(
         raw_data_path,
@@ -14,8 +24,19 @@ def prepare(raw_data_path, prepared_data_path):
     # Simplify species names by removing the prefix "Iris-"
     iris["species"] = iris["species"].apply(lambda s: s.replace("Iris-", ""))
 
-    iris.to_csv(prepared_data_path)
+    # Split the data into training and test
+    X = iris.drop("species", axis=1)
+    X = X.to_numpy()[:, (2, 3)]
+    y = iris["species"]
+
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.5, random_state=42
+    )
+
+    # Save the training and test data to CSV files
+    save_as_csv(X_train, y_train, prepared_data_dir / "train.csv")
+    save_as_csv(X_test, y_test, prepared_data_dir / "test.csv")
 
 
 if __name__ == "__main__":
-    prepare(raw_data_path=RAW_DATA_PATH, prepared_data_path=PREPARED_DATA_PATH)
+    prepare(raw_data_path=RAW_DATA_PATH, prepared_data_dir=PREPARED_DATA_PATH)
